6be95b91c7fb4ecf32378983f92fbce126e2e59b
Shruti Srivastava <shruti2000srivastava@gmail.com>
2026-02-08 17:16:09 +0530

    Add PySpark scripts for reading CSV data and saving as ORC and partitioned CSV

---
+++ pyspark-read/helper-functions.py
@@ -0,0 +1,11 @@
+dbutils.help()
+
+dbutils.fs.help()
+
+
+help(dbutils.fs.cp)
+
+
+display(dbutils.fs.ls("dbfs:/Volumes/population_metrics/landing/datasets/countries_dataset/csv_data/countries_population_partitioned/region_id=30/"))
+
+display(dbutils.fs.head("dbfs:/Volumes/population_metrics/landing/datasets/countries_dataset/csv_data/countries_population_partitioned/region_id=30/part-00000-tid-4367162852616795178-d62776f0-3395-4d9f-a9e2-d925e0fbad94-129-3.c000.csv"))
---
+++ pyspark-read/orc-read.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("Spark DataFrames").getOrCreate()
+
+from pyspark.sql.types import StructType,StructField,IntegerType,StringType
+
+path="/Volumes/population_metrics/landing/datasets/countries_dataset/csv_data/countries_population/countries_population.csv"
+
+schema = StructType(
+    [
+        StructField("country_id",IntegerType(),False),
+        StructField("name",StringType(),True),
+        StructField("nationality",StringType(),True),
+        StructField("country_code",StringType(),True),
+        StructField("iso_alpha2",StringType(),True),
+        StructField("capital",StringType(),True),
+        StructField("population",IntegerType(),True),
+        StructField("area_km2",IntegerType(),True),
+        StructField("region_id",IntegerType(),True),
+        StructField("sub_region_id",IntegerType(),True)
+    ]
+)
+
+df = spark.read.format("csv").schema(schema).option("header","true").load(path)
+
+# write contents of df into ORC
+df.write.format("orc").mode("overwrite").save("/Volumes/population_metrics/landing/datasets/output_dataset/orc/countries_population")
+
+#read from orc into a df
+
+spark.read.format("orc").load("/Volumes/population_metrics/landing/datasets/output_dataset/orc/countries_population").display()
